e863b3f9420055dfb99ae198bbe3afac035e3c68
IbrahimRamadan03 <96305440+IbrahimRamadan03@users.noreply.github.com>
2025-01-31 13:02:12 +0200

    Initial Commit

---
+++ combineTrain/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  combineTrain
+//
+//  Created by Apple on 31/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
