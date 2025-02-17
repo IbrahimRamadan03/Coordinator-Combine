fc5aee89dfc02957440d4050eb550b58d2b09623
IbrahimRamadan03 <96305440+IbrahimRamadan03@users.noreply.github.com>
2025-02-17 17:19:31 +0200

    implement data mapper

---
+++ combineTrain/Common/Util/DataMapper.swift
@@ -0,0 +1,8 @@
+//
+//  DataMapper.dart.swift
+//  combineTrain
+//
+//  Created by Apple on 17/02/2025.
+//
+
+import Foundation
---
+++ combineTrain/Features/Login/Model/LoginModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginModel.swift
+//  combineTrain
+//
+//  Created by Apple on 17/02/2025.
+//
+
+import Foundation
